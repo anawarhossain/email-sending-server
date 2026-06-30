d89ce8ca86fb8b8ba91b14294d53df3c4c32f6ec
Anawar Hossain <anwarhossainhossain01@gmail.com>
2026-06-30 12:59:20 +0600

    Set up Express server with CORS and dotenv configuration

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
-const port = 5000;
+require("dotenv").config();
+const port = process.env.PORT;
+
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.post("/api/email-send", (req, res) => {
+    try {
+        console.log(req.body);
+        res.status(200).send("Email sent successfully")
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+
+})
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
